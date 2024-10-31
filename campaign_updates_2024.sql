8c611314f60be3ca2fc76b417749197dbf4f4a87
jimendoza <90921656+jimendoza@users.noreply.github.com>
2024-10-31 16:35:30 -0400

    Update campaign_updates_2024.sql
    Added a commented out section that provides definitions of our 2024 Campaigns.

--- campaign_updates_2024.sql
+++ campaign_updates_2024.sql
@@ -47,3 +47,27 @@ WHERE batch_id IN ('5e0bf8f6-0cf7-422b-bbf3-93669dda4009', '5e0bf8f6-0cf7-422b-b
 ; 
 
 */
+/* 
+-- 2024 STATE VOICES CAMPAIGN DEFINITIONS
+-- 2024 Direct Community Service - Civic Engagement programming for mutual aid, disaster response 
+and relief or directing people to other public resources
+-- 2024 Policy Advocacy & Education - Civic engagement programming to educate and advocate for local, 
+state or federal public policy
+-- 2024 Special Election - Civic engagement activity programming to inform and mobilize voters to 
+participate in a state or local special election in 2024
+-- 2024 Statewide Primary/Caucus Election - Civic engagement activity to inform and mobilize voters 
+to participate in a statewide primary election or statewide caucus in 2024
+note: if your state had a statewide presidential primary and statewide primary election, please work 
+with the national data team on creating the appropriate campaign
+-- 2024 Voter Registration - Civic engagement activity focused on voter registration and programs, 
+such as: voter registration update programs for inactive voters, phone banking new registrants to 
+confirm the opt-in or voter registration, etc
+-- 2024 Local Election - Civic engagement activity programming to inform and mobilize voters to 
+participate in a local election in 2024 (i.e. school district millage, local elected official recall)
+-- 2024 Redistricting - civic engagement activity focused on educating, training and mobilizing 
+voters around redistricting efforts, events and policy
+-- 2024 Statewide General Election - Civic engagement activity to inform and mobilize voters to 
+participate in a statewide general election in 2024
+-- 2024 Voter Mobilization - Civic engagement activity to mobilize voters for an event, rally or 
+other non-partisan initiative
+*/
